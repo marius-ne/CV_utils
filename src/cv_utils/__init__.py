b92f34d9bba589877111d9c12e3ebeae27464848
Marius Neuhalfen <m.e.neuhalfen@t-online.de>
2025-05-25 00:39:55 +0300

    initial commit

---
+++ src/cv_utils/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.0"
+
+
+# Add here what user is going to be able to import "from package import ..."
+# from .core import CoreClass
+# from .utils import helper_function
+
+
+# Add here what user gets when doing "from package import *"
+# __all__ = ["CoreClass", "helper_function"]
