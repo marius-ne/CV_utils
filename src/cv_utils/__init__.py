eba993e868b881d84d8470ca0b3095dd524c3e0c
Marius Neuhalfen <m.e.neuhalfen@t-online.de>
2025-05-30 21:40:47 +0300

    fixed keypoint imports2

--- src/cv_utils/__init__.py
+++ src/cv_utils/__init__.py
@@ -8,5 +8,5 @@ from .keypoints import draw_kps_on_img, farthest_point_sampling, select_farthest
 # from .utils import helper_function
 
 # Add here what user gets when doing "from package import *"
-__all__ = ["render_depth_map", "invert_pose", "triangulate", "depth_estimate", "project_points", "draw_kps_on_img",
+__all__ = ["render_depth_map", "invert_pose", "triangulate", "depth_estimate", "project_points",
             "draw_kps_on_img", "farthest_point_sampling", "select_farthest_keypoints_from_obj", "add_visibility_to_keypoints"]
