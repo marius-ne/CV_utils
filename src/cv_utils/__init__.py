632d534cc1fd2e6caa0daf86290f7471582b3ca7
Marius Neuhalfen <m.e.neuhalfen@t-online.de>
2025-05-30 22:26:38 +0300

    added angle func

--- src/cv_utils/__init__.py
+++ src/cv_utils/__init__.py
@@ -2,11 +2,11 @@ __version__ = "0.1.0"
 
 
 # Add here what user is going to be able to import "from package import ..."
-from .core import render_depth_map, invert_pose, triangulate, depth_estimate, project_points
+from .core import render_depth_map, invert_pose, triangulate, depth_estimate, project_points, angle_between_vectors
 from .keypoints import draw_kps_on_img, farthest_point_sampling, select_farthest_keypoints_from_obj, add_visibility_to_keypoints
 
 # from .utils import helper_function
 
 # Add here what user gets when doing "from package import *"
-__all__ = ["render_depth_map", "invert_pose", "triangulate", "depth_estimate", "project_points",
+__all__ = ["render_depth_map", "invert_pose", "triangulate", "depth_estimate", "project_points", "angle_between_vectors"
             "draw_kps_on_img", "farthest_point_sampling", "select_farthest_keypoints_from_obj", "add_visibility_to_keypoints"]
